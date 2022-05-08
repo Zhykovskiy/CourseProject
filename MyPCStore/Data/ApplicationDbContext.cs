b5bd1e448a53702212940e64d510fc443c1694c3
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-05-08 21:15:04 +0300

    Created model Category

---
+++ MyPCStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPCStore.Models;
+
+namespace MyPCStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
