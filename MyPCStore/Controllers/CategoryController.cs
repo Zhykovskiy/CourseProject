7f0fbc3d00dae15f108564bad9910f0e976e9053
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-05-09 18:42:39 +0300

    Added validation for adding categories

--- MyPCStore/Controllers/CategoryController.cs
+++ MyPCStore/Controllers/CategoryController.cs
@@ -32,9 +32,14 @@ namespace MyPCStore.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
         }
     }
 }
--- MyPCStore/Models/Category.cs
+++ MyPCStore/Models/Category.cs
@@ -7,9 +7,12 @@ namespace MyPCStore.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage ="Display Order for catedory must be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
