353a17129fa0436d9f6d85d965efad5577aa9973
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-05-09 15:46:29 +0300

    Created adding categories

--- MyPCStore/Controllers/CategoryController.cs
+++ MyPCStore/Controllers/CategoryController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPCStore.Data;
+using MyPCStore.Models;
+using System.Collections.Generic;
 
 namespace MyPCStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> objList = _db.Category;
+
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
