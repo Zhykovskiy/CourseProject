2cdd4fa043d6b1735b2f885d29b510364ad97ec8
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-05-25 17:07:23 +0300

    Modified product model

--- MyPCStore/Models/Product.cs
+++ MyPCStore/Models/Product.cs
@@ -10,6 +10,7 @@ namespace MyPCStore.Models
 
         [Required]
         public string Name { get; set; }
+        public string ShortDesc { get; set; }
         public string Description { get; set; }
 
         [Range(1,int.MaxValue)]
