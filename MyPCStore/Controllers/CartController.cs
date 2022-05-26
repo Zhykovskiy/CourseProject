a48c4fbb8ac01e385bbbf67b94c8968efdb9721d
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2022-05-26 21:56:39 +0300

    Added page Summary

--- MyPCStore/Controllers/CartController.cs
+++ MyPCStore/Controllers/CartController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyPCStore.Data;
 using MyPCStore.Models;
+using MyPCStore.Models.ViewModels;
 using MyPCStore.Utility;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace MyPCStore.Controllers
 {
@@ -13,6 +15,8 @@ namespace MyPCStore.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDbContext db)
         {
@@ -32,6 +36,40 @@ namespace MyPCStore.Controllers
             IEnumerable<Product> productList = _db.Product.Where(u => prodInCart.Contains(u.Id));   
             
             return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = productList
+            };
+
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)
